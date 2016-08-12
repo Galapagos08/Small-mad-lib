bbb78580384e3807753af8098c2955255c96cfdc
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-12 14:56:25 -0400

    Initial Commit

---
+++ Small mad-lib game/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Small mad-lib game
+//
+//  Created by Dan Esrey on 2016/12/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
